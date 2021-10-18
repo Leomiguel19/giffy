fcc647c98620ed06a31ad81f5eabff861f629222
Leomiguel19 <leomiguel1907@gmail.com>
2021-10-17 23:53:29 -0400

    Mostrando un monton de gifs

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 import React, {useState, useEffect} from 'react';
 import './App.css';
 
-const GIFS = [
-  'https://media3.giphy.com/media/l0Iyoy5VglR6QKJiw/giphy.gif?cid=ecf05e477h6nc0lu202xl0icph383ido8ezi44qh307lstyg&rid=giphy.gif&ct=g',
-  'https://media0.giphy.com/media/3o7bugwhhJE9WhxkYw/giphy.gif?cid=ecf05e477h6nc0lu202xl0icph383ido8ezi44qh307lstyg&rid=giphy.gif&ct=g'
-]
-
-const DIFFERENT_GIFS = [
-  'https://media2.giphy.com/media/xUPGcC4A6ElcqtUJck/giphy.gif?cid=ecf05e47viuqcm01tf344df1xem1vlb3xakgk2ywvj4gcgzu&rid=giphy.gif&ct=g'  
+const apiURL = [
+  'https://api.giphy.com/v1/gifs/search?api_key=SLIWUFflTX1UgjsJDduoKU2AnTN4pNt1&q=shingeki+no+kyojin&limit=25&offset=0&rating=g&lang=en'
 ]
 
 function App() {
-  const [gifs, setGifs] = useState(GIFS)
+  const [gifs, setGifs] = useState([])
 
   useEffect(function () {
-    setGifs(DIFFERENT_GIFS)
-  }, [])
+    console.log('actualizando los gifs')
+    // setGifs(DIFFERENT_GIFS)
+    fetch(apiURL)
+      .then(res => res.json())
+      .then(response => {
+        const {data} = response
+        const gifs = data.map(image => image.images.downsized_medium.url)
+        setGifs(gifs)
+      })
+  },[])
 
   return (
     <div className="App">
