595128a7af1edf37a9a0cfed19f32eb89eddcd5f
Leomiguel19 <leomiguel1907@gmail.com>
2021-10-18 00:17:51 -0400

    Extrayendo la lógica a un servicio

--- src/App.js
+++ src/App.js
@@ -1,35 +1,29 @@
-import React, {useState, useEffect} from 'react';
-import './App.css';
-
-const apiURL = [
-  'https://api.giphy.com/v1/gifs/search?api_key=SLIWUFflTX1UgjsJDduoKU2AnTN4pNt1&q=shingeki+no+kyojin&limit=25&offset=0&rating=g&lang=en'
-]
+import React, { useState, useEffect } from "react"
+import "./App.css"
+import getGifs from "./services/getGifs"
 
 function App() {
-  const [gifs, setGifs] = useState([])
+  	const [gifs, setGifs] = useState([])
 
-  useEffect(function () {
-    console.log('actualizando los gifs')
-    // setGifs(DIFFERENT_GIFS)
-    fetch(apiURL)
-      .then(res => res.json())
-      .then(response => {
-        const {data} = response
-        const gifs = data.map(image => image.images.downsized_medium.url)
-        setGifs(gifs)
-      })
-  },[])
+	useEffect(function () {
+    	getGifs({keyword: 'rick'}).then(gifs => setGifs(gifs))
+  	}, [])
 
-  return (
-    <div className="App">
-      <section className="App-content">
-        {
-          gifs.map(singleGif => <img src={singleGif} alt="" />)
-        }   
-        {/* <button onClick={() => setGifs(DIFFERENT_GIFS)}>Cambiar gifs</button> */}
-      </section>
-    </div>
-  );
+  	return (
+    	<div className="App">
+      		<section className="App-content">
+			{
+				gifs.map(singleGif => {
+					return <div>
+						<h4>{singleGif.title}</h4>
+						<small>{singleGif.id} </small>
+						<img src={singleGif.url} alt={singleGif.title} />
+					</div>
+				})
+			}
+			</section>
+		</div>
+  	);
 }
 
 export default App;
