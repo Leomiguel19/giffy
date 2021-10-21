026f539490227be1d69dfe95fc4dd19ae9bbb05d
Leomiguel19 <leomiguel1907@gmail.com>
2021-10-20 23:55:05 -0400

    Conseguir que sea una SPA sin recargas

--- src/App.js
+++ src/App.js
@@ -2,17 +2,17 @@ import React from "react"
 import "./App.css"
 import ListOfGifs from "./components/ListOfGifs"
 
-import { Route } from "wouter"
+import { Route, Link } from "wouter"
 
 function App() {
   	return (
     	<div className="App">
       		<section className="App-content">
 				<h1>App</h1>
-				<a href='/gif/tokyo revenger'>Gif de tokyo revenger</a>
-				<a href='/gif/hunterxhunter'>Gif de hunterxhunter</a>
-				<a href='/gif/god of war'>Gif de god of war</a>
-				<a href='/gif/horizon zero dawn'>Gif de horizon zero dawn</a>
+				<Link href='/gif/tokyo revenger'>Gif de tokyo revenger</Link>
+				<Link href='/gif/hunterxhunter'>Gif de hunterxhunter</Link>
+				<Link href='/gif/god of war'>Gif de god of war</Link>
+				<Link href='/gif/horizon zero dawn'>Gif de horizon zero dawn</Link>
 			  	<Route 
 					component={ListOfGifs}
 				  	path="/gif/:keyword" 			  
