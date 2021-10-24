a50f04639992049dce21c9103ebc41539e464415
Leomiguel19 <leomiguel1907@gmail.com>
2021-10-23 23:25:25 -0400

    Añadiendo un form que haga de buscador de Gifs

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,21 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from "wouter"
 
 const POPULAR_GIFS = ["tokyo revenger", "hunterxhunter", "god of war", "horizon zero dawn", "shingeki no kyojin"]
 
 export default function Home() {
+    const [keyword, setKeyword] = useState('')
+
+    const handleSubmit = evt => {
+        evt.preventDefault()
+        // Navegar a otra ruta
+        console.log(keyword)
+    }
+
+    const handleChange = evt => {
+        setKeyword(evt.target.value)
+    }
+
     return (
         <>
-          <h3 className="App-title"> Los Gif mas populares</h3>  
-          <ul>
-            {POPULAR_GIFS.map((popularGif) => (
-                <li key={popularGif}>
-                    <Link to={`/search/${popularGif}`}>
-                        Gifs de {popularGif}
-                    </Link>
-                </li>
-            ))}
-          </ul>
+            <form onSubmit={handleSubmit} >
+                <input onChange={handleChange} type='text' value={keyword}/>
+            </form>
+            <h3 className="App-title"> Los Gif mas populares</h3> 
+            <ul>
+                {POPULAR_GIFS.map((popularGif) => (
+                    <li key={popularGif}>
+                        <Link to={`/search/${popularGif}`}>
+                            Gifs de {popularGif}
+                        </Link>
+                    </li>
+                ))}
+            </ul>
         </>
     )
 }
