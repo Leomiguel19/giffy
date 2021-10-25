7beef62fe179933ceb7f6c02755660f0b60e9aea
Leomiguel19 <leomiguel1907@gmail.com>
2021-10-24 23:49:24 -0400

    useGifs, haciendo que use el localStorage

--- src/hooks/useGifs.jsx
+++ src/hooks/useGifs.jsx
@@ -1,16 +1,21 @@
 import {useEffect, useState} from 'react'
 import getGifs from '../services/getGifs'
 
-export function useGifs ({ keyword }) {
+export function useGifs ({ keyword } = { keyword: null}) {
     const [loading, setLoading] = useState(false)
     const [gifs, setGifs] = useState([])
 
     useEffect(function () {
     setLoading(true)
-    getGifs({ keyword })
+    // Recuperamos la keyword del localstorage
+    const keywordToUse = keyword || localStorage.getItem('lastKeyword') || 'random'
+
+    getGifs({ keyword: keywordToUse })
         .then(gifs => {
-        setGifs(gifs)
-        setLoading(false)
+            setGifs(gifs)
+            setLoading(false)
+            // Guardamos la keyword en el localStorage
+            localStorage.setItem('lastKeyword', keyword)
         })
     }, [keyword])
 
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react'
 import { Link, useLocation } from "wouter"
+import ListOfGifs from '../../components/ListOfGifs'
+import { useGifs } from '../../hooks/useGifs'
 
 const POPULAR_GIFS = ["tokyo revenger", "hunterxhunter", "god of war", "horizon zero dawn", "shingeki no kyojin"]
 
@@ -7,7 +9,7 @@ export default function Home() {
     const [keyword, setKeyword] = useState('')
     const [path, pushLocation] = useLocation()
 
-    console.log(path)
+    const {loading, gifs} = useGifs()
 
     const handleSubmit = evt => {
         evt.preventDefault()
@@ -22,8 +24,11 @@ export default function Home() {
     return (
         <>
             <form onSubmit={handleSubmit} >
-                <input onChange={handleChange} type='text' value={keyword}/>
+                <input placeholder="Search a gif here..." onChange={handleChange} type='text' value={keyword}/>
+                <button>Buscar</button>
             </form>
+            <h3 className="App-title">Última búsqueda</h3> 
+            <ListOfGifs gifs={gifs} />
             <h3 className="App-title"> Los Gif mas populares</h3> 
             <ul>
                 {POPULAR_GIFS.map((popularGif) => (
