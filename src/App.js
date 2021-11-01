dcb0727a351adf7d496f591a96f6233efd173e79
leomiguel19 <leomiguel1907@gmail.com>
2021-10-31 21:43:08 -0400

    Usando el provider del Context

--- src/App.js
+++ src/App.js
@@ -3,27 +3,30 @@ import "./App.css"
 import SearchResults from "./pages/SearchResults";
 import Home from "./pages/Home";
 import Detail from './pages/Detail'
+import StaticContext from './context/StaticContext'
 import { Route, Link } from "wouter"
 
 function App() {
   	return (
-    	<div className="App">
-      		<section className="App-content">
-				<Link to="/">
-					<img className="App-logo" alt='Giffy logo'  src='/logo.png'/>
-				</Link>	
-				<Route 
-					component={Home}
-					path="/" />			
-				<Route
-					component={SearchResults}
-					path="/search/:keyword" />					
-				<Route 
-					component={Detail}
-					path="/gif/:id" />
-			</section>
-		</div>
-  	);
+		<StaticContext.Provider>
+			<div className="App">
+				<section className="App-content">
+					<Link to="/">
+						<img className="App-logo" alt='Giffy logo'  src='/logo.png'/>
+					</Link>	
+					<Route 
+						component={Home}
+						path="/" />			
+					<Route
+						component={SearchResults}
+						path="/search/:keyword" />					
+					<Route 
+						component={Detail}
+						path="/gif/:id" />
+				</section>
+			</div>
+		</StaticContext.Provider>
+  	)
 }
 
 export default App;
